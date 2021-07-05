07abf028a0dafd3fa4fa3d82137e60f3fcdc60b2
Sofi <tamara.sofinet@pm.me>
2021-07-05 16:25:38 +0200

    Integrate Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* refer for more details https://tailwindcss.com/docs/configuration */
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
